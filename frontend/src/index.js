8bca110baf7687a036adc59bab60fd58e8b7c0df
rasadregmi <regmirasad53@gmail.com>
2025-05-28 23:08:57 +0545

    Fixed extra white area

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 
+const setVh = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+setVh();
+window.addEventListener('resize', setVh);
+window.addEventListener('orientationchange', setVh);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
